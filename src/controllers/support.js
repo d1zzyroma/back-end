ee036646f5294e43dba27b651c6eb8bf6d8b5914
BilousPM <bilouspm@gmail.com>
2024-11-02 20:25:42 -0600

    Adds the ability to Send a message to supportю

--- src/controllers/support.js
+++ src/controllers/support.js
@@ -1,21 +1,44 @@
+import createHttpError from "http-errors";
+import { SMTP, TEMPLATES_DIR } from "../constants/index.js";
+import { env } from "../utils/env.js";
+import { sendEmailRequest } from "../utils/sendMail.js";
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import handlebars from 'handlebars';
+
 export const sendSupportMessageController = async (req, res) => {
   const email = req.body.email
-  console.log(email);
+  const supportEmail = "bilouspm@gmail.com"
+  //  const supportEmail = "taskpro.project@gmail.com"
 
-  // try {
-  //   await sendEmailRequest({
-  //     to: email,
-  //     from: env(SMTP.SMTP_FROM),
-  //     sudject: 'Reset your password',
-  //     html,
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-  //   throw createHttpError(
-  //     500,
-  //     'Failed to send the email, please try again later.',
-  //   );
-  // }
+  const sendEmailTemplatePath = path.join(TEMPLATES_DIR, 'support-email.html')
+
+  const templateSource = (
+    await fs.readFile(sendEmailTemplatePath)
+  ).toString();
+
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    email,
+    message: req.body.message
+
+  });
+
+
+  try {
+    await sendEmailRequest({
+      to: supportEmail,
+      from: env(SMTP.SMTP_FROM),
+      sudject: 'Reset your password',
+      html,
+    });
+  } catch (error) {
+    console.log(error);
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
 
   res.json({
     status:200,
