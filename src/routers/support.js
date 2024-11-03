68e7b5f89752239f914eb6a83cac82f632818a79
BilousPM <bilouspm@gmail.com>
2024-11-02 19:12:19 -0600

    create rout send message

--- src/routers/support.js
+++ src/routers/support.js
@@ -3,11 +3,14 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { sendSupportMessageController } from '../controllers/support.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { supportSchema } from '../validations/support.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const supportRouter = Router();
 
+supportRouter.use('/', authenticate)
+
 supportRouter.post(
-  '/',
+  '/send-message',
   ctrlWrapper(sendSupportMessageController),
   validateBody(supportSchema),
 );
