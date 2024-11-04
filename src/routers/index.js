a9495b4f8d239178edfd477c4c361cfdf268d3c4
BilousPM <bilouspm@gmail.com>
2024-11-03 22:37:18 -0700

    fixed create board and get boards

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,10 +3,12 @@ import authRouter from './auth.js';
 import cardsRouter from './card.js';
 import themeRouter from './theme.js';
 import supportRouter from './support.js';
+import boardsRouter from './boards.js';
 
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/boards', boardsRouter);
 router.use('/card', cardsRouter);
 router.use('/theme', themeRouter);
 router.use('/support', supportRouter);
