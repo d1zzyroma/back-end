910928ea2c59d1a30370dd4ad67f951383448d76
BilousPM <bilouspm@gmail.com>
2024-11-04 10:03:00 -0700

    change user schema

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    avatarURL: { type: String, required: true },
+    avatarURL: { type: String},
     theme: {
       type: String,
       required: true,
