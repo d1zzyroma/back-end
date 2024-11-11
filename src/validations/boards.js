70a962b6b52258b5cfffc98e6eb233a8782c3540
BilousPM <bilouspm@gmail.com>
2024-11-10 21:50:46 -0700

    rebuild swagger docs

--- src/validations/boards.js
+++ src/validations/boards.js
@@ -1,20 +1,9 @@
-// src/validation/boards.js
-
 import Joi from 'joi';
 
-// export const createBoardSchema = Joi.object({
-//   title: Joi.string().min(3).max(30).required().messages({
-//     'string.base': 'Username should be a string', // Кастомізація повідомлення для типу "string"
-//     'string.min': 'Username should have at least {#limit} characters',
-//     'string.max': 'Username should have at most {#limit} characters',
-//     'any.required': 'Username is required',
-//   }),
-// });
 export const createBoardSchema = Joi.object({
   title: Joi.string().min(3).max(30).required(),
   background: Joi.string(),
   icon: Joi.string(),
-  // filter: Joi.string(),// Не використовуємо
 });
 
 export const updateBoardSchema = Joi.object({
@@ -23,15 +12,15 @@ export const updateBoardSchema = Joi.object({
   icon: Joi.string(),
 });
 
-const dataToValidate = {
-  title: 'Project offise',
-};
+// const dataToValidate = {
+//   title: 'Project offise',
+// };
 
-const validationResult = createBoardSchema.validate(dataToValidate, {
-  abortEarly: false,
-});
-if (validationResult.error) {
-  console.error(validationResult.error.message);
-} else {
-  console.log('Data is valid!');
-}
+// const validationResult = createBoardSchema.validate(dataToValidate, {
+//   abortEarly: false,
+// });
+// if (validationResult.error) {
+//   console.error(validationResult.error.message);
+// } else {
+//   console.log('Data is valid!');
+// }
