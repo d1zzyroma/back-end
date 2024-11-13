4223bf044b28859da454b6b56ec938634e61693e
BilousPM <bilouspm@gmail.com>
2024-11-13 14:47:01 -0700

    clean files

--- src/utils/googleOauth.js
+++ src/utils/googleOauth.js
@@ -2,7 +2,7 @@ import { OAuth2Client } from 'google-auth-library';
 import fs from 'node:fs';
 import path from 'node:path';
 import { env } from './env.js';
-import { GOOGLE_OAUTH, MONGO_DB_VARS } from '../constants/index.js';
+import { GOOGLE_OAUTH } from '../constants/index.js';
 import createHttpError from 'http-errors';
 
 const googleConfigPath = path.join(process.cwd(), 'google.json');
@@ -11,12 +11,6 @@ const googleOauthParams = JSON.parse(
   fs.readFileSync(googleConfigPath).toString(),
 );
 
-// const oauthClient = new OAuth2Client({
-//   projectId: googleOauthParams.web.project_id,
-//   clientId: env(MONGO_DB_VARS.GOOGLE_OAUTH_CLIENT_ID),
-//   clientSecret: env(MONGO_DB_VARS.GOOGLE_OAUTH_SECTRET),
-//   redirectUri: env(MONGO_DB_VARS.GOOGLE_OAUTH_REDIRECT_URI),
-// });
 const oauthClient = new OAuth2Client({
   projectId: googleOauthParams.web.project_id,
   clientId: env(GOOGLE_OAUTH.CLIENT_ID),
