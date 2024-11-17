e1c22f4271ffa3798ee3f7ca62bc3c8070127284
d1zzyroma <nataliawologin@gmail.com>
2024-11-17 13:26:05 +0100

    321

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,44 +1,44 @@
-import path from 'node:path';
+  import path from 'node:path';
 
-export const ENV_VARS = {
-  PORT: 'PORT',
-};
+  export const ENV_VARS = {
+    PORT: 'PORT',
+  };
 
-export const MONGO_DB_VARS = {
-  MONGODB_USER: 'MONGODB_USER',
-  MONGODB_PASSWORD: 'MONGODB_PASSWORD',
-  MONGODB_URL: 'MONGODB_URL',
-  MONGODB_DB: 'MONGODB_DB',
-};
+  export const MONGO_DB_VARS = {
+    MONGODB_USER: 'MONGODB_USER',
+    MONGODB_PASSWORD: 'MONGODB_PASSWORD',
+    MONGODB_URL: 'MONGODB_URL',
+    MONGODB_DB: 'MONGODB_DB',
+  };
 
-export const GOOGLE_OAUTH = {
-  CLIENT_ID: 'GOOGLE_OAUTH_CLIENT_ID',
-  SECTRET: 'GOOGLE_OAUTH_SECTRET',
-  REDIRECT_URI: 'GOOGLE_OAUTH_REDIRECT_URI'
-};
+  export const GOOGLE_OAUTH = {
+    CLIENT_ID: 'GOOGLE_OAUTH_CLIENT_ID',
+    SECTRET: 'GOOGLE_OAUTH_SECTRET',
+    REDIRECT_URI: 'GOOGLE_OAUTH_REDIRECT_URI'
+  };
 
-export const CLOUDINARY = {
-  CLOUD_NAME: 'CLOUD_NAME',
-  API_KEY: 'API_KEY',
-  API_SECRET: 'API_SECRET',
-};
+  export const CLOUDINARY = {
+    CLOUD_NAME: 'CLOUD_NAME',
+    API_KEY: 'API_KEY',
+    API_SECRET: 'API_SECRET',
+  };
 
-export const SMTP = {
-  SMTP_HOST: 'SMTP_HOST',
-  SMTP_PORT: 'SMTP_PORT',
-  SMTP_USER: 'SMTP_USER',
-  SMTP_PASSWORD: 'SMTP_PASSWORD',
-  SMTP_FROM: 'SMTP_FROM',
-};
+  export const SMTP = {
+    SMTP_HOST: 'SMTP_HOST',
+    SMTP_PORT: 'SMTP_PORT',
+    SMTP_USER: 'SMTP_USER',
+    SMTP_PASSWORD: 'SMTP_PASSWORD',
+    SMTP_FROM: 'SMTP_FROM',
+  };
 
-export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+  export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 
-export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
+  export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
 
-export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
-export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+  export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+  export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
-// export const SORT_ORDER = {
-//   ASC: 'asc',
-//   DESC: 'desc',
-// };
+  // export const SORT_ORDER = {
+  //   ASC: 'asc',
+  //   DESC: 'desc',
+  // };
